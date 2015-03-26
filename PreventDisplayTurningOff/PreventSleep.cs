3fb41a4f9f2a8e96b90d357aceac95ddb698449d
unknown <pkuma@pkuma-dsk-vs.fareast.corp.microsoft.com>
2015-03-26 18:57:26 +0530

    submitting preventdisplayturningoff

---
+++ PreventDisplayTurningOff/PreventSleep.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+
+namespace PreventDisplayTurningOff
+{
+    class PreventSleep
+    {
+        [FlagsAttribute]
+        public enum EXECUTION_STATE : uint
+        {
+            ES_SYSTEM_REQUIRED = 0x00000001,
+            ES_DISPLAY_REQUIRED = 0x00000002,
+            // Legacy flag, should not be used.
+            // ES_USER_PRESENT   = 0x00000004,
+            ES_AWAYMODE_REQUIRED = 0x00000040,
+            ES_CONTINUOUS = 0x80000000,
+        }
+
+        public static class SleepUtil
+        {
+            [DllImport("kernel32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+            public static extern EXECUTION_STATE SetThreadExecutionState(EXECUTION_STATE esFlags);
+        }
+
+        public static void PreventMachineFromSleeping()
+        {
+            //Away mode for Windows >= Vista
+            if (SleepUtil.SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS | 
+                                            EXECUTION_STATE.ES_DISPLAY_REQUIRED | 
+                                            EXECUTION_STATE.ES_SYSTEM_REQUIRED | 
+                                            EXECUTION_STATE.ES_AWAYMODE_REQUIRED) == 0)
+            {
+                //Windows < Vista, forget away mode
+                EXECUTION_STATE temp = SleepUtil.SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS
+                    | EXECUTION_STATE.ES_DISPLAY_REQUIRED
+                    | EXECUTION_STATE.ES_SYSTEM_REQUIRED);
+
+                temp = EXECUTION_STATE.ES_CONTINUOUS;
+            }
+        }
+
+        public static void RestoreSetting()
+        {
+            SleepUtil.SetThreadExecutionState(EXECUTION_STATE.ES_CONTINUOUS);
+        }
+    }
+}
---
+++ PreventDisplayTurningOff/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Threading;
+using System.Runtime.InteropServices;
+
+namespace PreventDisplayTurningOff
+{
+    class Program
+    {
+        public static ConsoleColor oldClr;
+
+        static void Main(string[] args)
+        {
+            char[] progress = { '|', '/', '-', '\\' };
+
+            oldClr = Console.ForegroundColor;
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("This program will prevent the display from turning off.");
+            Console.WriteLine("Press ctrl + c to close this program..");
+
+
+            Console.CancelKeyPress += new ConsoleCancelEventHandler(OnConsoleCancelKeyPress);
+
+            PreventSleep.PreventMachineFromSleeping();
+            
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine();
+
+            int top = Console.CursorTop;
+            int left = Console.CursorLeft;
+            
+            int idx = 0;
+            int midx = 0;
+            while (true)
+            {
+                Thread.Sleep(200);
+
+                midx = progress.Length - (idx + 1);
+                Console.Write(" [ " + progress[idx] + " ]");
+                Console.WriteLine(" [ " + progress[midx] + " ]");
+
+                Console.SetCursorPosition(left, top);
+
+                idx = (++idx) % progress.Length;
+            }
+        }
+
+        static void OnConsoleCancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            Console.ForegroundColor = oldClr;
+            PreventSleep.RestoreSetting();
+        }
+    }
+}
